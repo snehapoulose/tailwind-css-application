0234b0dcbc49cc4f11d8ed2322894646086c9c68
snehapoulose <snehapoulose1999@gmail.com>
2025-09-02 21:10:25 +0530

    --updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import FormSection from './components/FormSection'
+import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
 
 function App() {
@@ -7,6 +8,7 @@ return (
     <>
       <NavBar/>
       <FormSection/>
+      <HeroSection/>
     </>
   )
 }
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <div>
+      <div className='columns-5 gap-2'>
+        <div>8</div>
+        <div>7</div>
+        <div>4</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </div>
+  )
+}
